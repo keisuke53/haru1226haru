b9d9fd0c047088bb2da77a5bd24a4f77c875bbc8
keisuke53 <haru1226hito@gmail.com>
2024-08-13 19:44:25 +0900

    “haruhito”

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def top():
-    return "Haruhito"
+    return render_template("haruhito.html")
 
 app.run(host="0.0.0.0")
