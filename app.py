975ecf8bc61e1cc4eb9aaf5fdb56e10cde5916db
keisuke53 <haru1226hito@gmail.com>
2024-09-24 20:34:44 +0900

    “haruhito”

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask import request
+
+from database import Person
 
 app = Flask(__name__)
 
@@ -7,4 +11,16 @@ app = Flask(__name__)
 def top():
     return render_template("haruhito.html")
 
-app.run(host="0.0.0.0")
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+@app.route("/create", methods={"POST"})
+def create():
+    Person.create(
+        name=request.form["name"],
+        emotion=request.form["emotion"],
+        age=request.form["age"],
+    )
+    return redirect("/")
+app.run(host="0.0.0.0", debug=True)
